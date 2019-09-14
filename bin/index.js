b9af46dc3e55516030a6f72f85de067832298e55
Steven Mercatante <steven.mercatante@gmail.com>
2019-09-14 16:35:12 -0400

    clean up

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 
-const yargs = require("yargs");
-
-yargs
+require("yargs")
   .option("dir", { alias: "d", description: "directory where posts live" })
   .command(require("./commands/posts"))
   .command(require("./commands/postStats"))
