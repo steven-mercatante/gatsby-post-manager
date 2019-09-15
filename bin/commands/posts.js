6eb6ac4b3912ded7143e157bab17bee6da711682
Steven Mercatante <steven.mercatante@gmail.com>
2019-09-15 09:18:32 -0400

    require colors in posts cmd

--- bin/commands/posts.js
+++ bin/commands/posts.js
@@ -1,4 +1,5 @@
 const {
+  colors,
   getAllPostsData,
   getPostsDir,
   getPublishedPosts,
--- bin/utils.js
+++ bin/utils.js
@@ -149,6 +149,7 @@ function getPostsDir(dir = ".") {
 }
 
 module.exports = {
+  colors,
   getAllPostsData,
   getCurrentDate,
   getPostsDir,
